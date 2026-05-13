61e8e1e2f1ec3d6aad787033e5139abfec34695e
deionsebastian97-collab <deionsebastian97@gmail.com>
2026-05-13 01:46:51 -0400

    Add Home component with styled layout

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{background:"#080808",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <div style={{color:"#C9A84C",fontFamily:"serif",fontSize:48,letterSpacing:8}}>
+        VAULTFLOW
+      </div>
+    </div>
+  )
+}
