dcb7e4d2b34d5786d3ae41d1c3819c6f9bfd56b6
deionsebastian97-collab <deionsebastian97@gmail.com>
2026-05-13 01:54:29 -0400

    Delete app/page.js

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{background:"#080808",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
-      <div style={{color:"#C9A84C",fontFamily:"serif",fontSize:48,letterSpacing:8}}>
-        VAULTFLOW
-      </div>
-    </div>
-  )
-}
